55aab45953f4326df4303d002a6686b9bfea737a
Megan Comeau <mlcomeau99@gmail.com>
2020-04-29 19:19:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -43,8 +43,10 @@ class Triangle
     (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
   end
 
+  class TriangleError < StandardError
+  end 
+  
+
 end
 
-class TriangleError < StandardError
 
-end
