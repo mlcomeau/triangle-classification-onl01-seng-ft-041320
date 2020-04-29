484c0f4f3162523c1cb535b8491f970464d84726
Megan Comeau <mlcomeau99@gmail.com>
2020-04-29 19:06:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,12 @@
 class Triangle
   def initialize(side1, side2, side3)
     @side1 = side1
-    @side2 = side2 
-    @side3 = side3 
-  end 
+    @side2 = side2
+    @side3 = side3
+  end
 
-  def type 
-  end 
+  def type
+  end
 
-  
+
 end
